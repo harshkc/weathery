52d8d45536af2f29b6598267cd2f6cd69b3262e2
Harsh Choudhary <harsh.hkc1006@gmail.com>
2021-04-26 20:53:07 +0530

    UI: Setup files for presentation screens :art:  (#4)
    * added bloc observer
    
    * segregated main file and added bloc provider
    
    * ui:setup weather widget
    
    * quickfixes

---
+++ lib/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:bloc_weather/bloc/weather_bloc.dart';
+import 'package:bloc_weather/repository/repository.dart';
+import 'package:bloc_weather/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class App extends StatelessWidget {
+  final WeatherRepository weatherRepository;
+  const App({Key key, @required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weathery',
+      home: BlocProvider(
+        create: (context) => WeatherBloc(weatherRepository: weatherRepository),
+        child: Weather(),
+      ),
+    );
+  }
+}
